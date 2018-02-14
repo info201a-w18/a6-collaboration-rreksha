30b05c6a5ee2188fc8f045c4cf34e70e223d1c58
RekshaRathnam <rreksha@uw.edu>
2018-02-14 14:20:46 -0800

    Testing changes

--- HW6.R
+++ HW6.R
@@ -9,7 +9,13 @@ library("dplyr")
 info <- read.csv('data/intro-survey.csv')
 
 # Summary Statistics Description 
-mean.program.exp <- summarise(info, mean(ï..programming_exp))
+mean.program.exp <- summarise(info, mean(ï..programming_exp)) 
+from.washington <- count(filter(info, washington_born == "Yes"))
+windows.count <- count(filter(info, os == "Windows"))
+mac.count <- count(filter(info, os == "Mac OS X"))
+coffee.max <- summarise(info, max(coffee_cups))
+seahawks.superfans <- count(filter(info, seahawks_fan == "Yes!!!"))
+percent.info.interest <- ((count(filter(info, info_interest == "Yes"))) / (count(info))) * 100
 
 # Response Statistics Table 
 # First Visualization 
