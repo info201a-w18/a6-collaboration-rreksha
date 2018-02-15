aba824683921a7c95b0c8541e452d0fed0f4ac4b
RekshaRathnam <rreksha@uw.edu>
2018-02-15 09:56:30 -0800

    Add section 2 code

--- HW6.R
+++ HW6.R
@@ -14,3 +14,25 @@ mean.program.exp <- summarise(info, mean(ï..programming_exp))
 # Response Statistics Table 
 # First Visualization 
 # Second Visualization 
+program.exp <- select(info,ï..programming_exp, cli_exp, vcs_exp, md_exp, r_exp, web_exp)
+
+rank.num <- function(rank){
+  rank.vector <- c((nrow(filter(program.exp, ï..programming_exp == rank))),
+                   (nrow(filter(program.exp, cli_exp == rank))),
+                   (nrow(filter(program.exp, vcs_exp == rank))),
+                   (nrow(filter(program.exp, md_exp == rank))),
+                   (nrow(filter(program.exp, r_exp == rank))),
+                   (nrow(filter(program.exp, web_exp == rank))))
+  return (rank.vector)
+}
+
+
+rank.one <- rank.num(1)
+rank.two <- rank.num(2)
+rank.three <- rank.num(3)
+rank.four <- rank.num(4)
+rank.five <- rank.num(5)
+
+exp.data <- data.frame(rank.one, rank.two, rank.three, rank.four, rank.five)
+names <- c("Program", "Commant_Line", "Version_Control", "Markdown", "R", "Web")
+row.names(exp.data) <- names
