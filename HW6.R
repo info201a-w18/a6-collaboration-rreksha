925ac4a9abbe7b559192a466ea02365e9ff91709
Anant Rajeev <anant.rajeev@gmail.com>
2018-02-15 16:40:28 -0800

    Add in first data visualization

--- HW6.R
+++ HW6.R
@@ -4,13 +4,28 @@
 # 2/12/18 
 # INFO 201 HW 6 
 
+# install.packages("ggplot2")
+
+library("knitr")
 library("dplyr")
+library("ggplot2")
+library("kableExtra")
+
 
 info <- read.csv('data/intro-survey.csv')
 
 # Summary Statistics Description 
-mean.program.exp <- summarise(info, mean(ï..programming_exp))
 
-# Response Statistics Table 
-# First Visualization 
-# Second Visualization 
+#### Response Statistics Table ####
+
+# First Visualization
+ggplot(data = info) +
+  geom_smooth(mapping = aes(x = programming_exp, y = cli_exp, color = "red")) +
+  geom_smooth(mapping = aes(x = programming_exp, y = r_exp, color = "black")) + 
+  geom_smooth(mapping = aes(x = programming_exp, y = web_exp, color = "blue")) + 
+  xlab("Programming Experience") +
+  ylab("Experience") +
+  ggtitle("Overall Programming Experience vs Specific Experience") +
+  scale_color_manual(labels = c("Web","R","Command Line"), values = c("red","black","blue"))
+
+# Second Visualization
