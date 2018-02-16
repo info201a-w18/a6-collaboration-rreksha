8c161409da3ec46420d8918e9170c1eb5f06d000
Anant Rajeev <anant.rajeev@gmail.com>
2018-02-15 16:44:51 -0800

    Add code for second data viz

--- HW6.R
+++ HW6.R
@@ -4,13 +4,25 @@
 # 2/12/18 
 # INFO 201 HW 6 
 
+# install.packages("ggplot2")
+
+library("knitr")
 library("dplyr")
+library("ggplot2")
+
 
 info <- read.csv('data/intro-survey.csv')
 
 # Summary Statistics Description 
-mean.program.exp <- summarise(info, mean(ï..programming_exp))
 
-# Response Statistics Table 
-# First Visualization 
-# Second Visualization 
+#### Response Statistics Table ####
+
+# First Visualization
+
+# Second Visualization
+info <- filter(info, coffee_cups < 80000)
+ggplot(data = info) + 
+  geom_point(mapping = aes(x = seahawks_fan, y = coffee_cups, color = washington_born)) +
+  xlab("Seahawks Fan") +
+  ylab("Coffee Cups") +
+  ggtitle("Coffee Cups vs. Seahawks Fans vs. Born in Washington")
