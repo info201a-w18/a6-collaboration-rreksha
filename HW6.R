79b814a3ee5ef2918176290a1d67b6042a5327da
Anant Rajeev <anant.rajeev@gmail.com>
2018-02-15 16:15:15 -0800

    Includes changes for data visualization section

--- HW6.R
+++ HW6.R
@@ -4,8 +4,11 @@
 # 2/12/18 
 # INFO 201 HW 6 
 
+# install.packages("ggplot2")
+
 library("knitr")
 library("dplyr")
+library("ggplot2")
 
 info <- read.csv('data/intro-survey.csv')
 
@@ -19,5 +22,25 @@ seahawks.superfans <- count(filter(info, seahawks_fan == "Yes"))
 percent.info.interest <- round((count(filter(info, info_interest == "Yes"))) / (count(info)), digits = 1) * 100
 
 # Response Statistics Table 
-# First Visualization 
-# Second Visualization 
+
+# First Visualization
+ggplot(data = info) +
+  geom_smooth(mapping = aes(x = programming_exp, y = cli_exp, color = "red")) +
+  geom_smooth(mapping = aes(x = programming_exp, y = r_exp, color = "black")) + 
+  geom_smooth(mapping = aes(x = programming_exp, y = web_exp, color = "blue")) + 
+  xlab("Programming Experience") +
+  ylab("Experience") +
+  ggtitle("Overall Programming Experience vs Specific Experience") +
+  scale_color_manual(labels = c("Web","R","Command Line"), values = c("red","black","blue"))
+
+# Second Visualization
+info <- filter(info, coffee_cups < 80000)
+ggplot(data = info) + 
+  geom_point(mapping = aes(x = seahawks_fan, y = coffee_cups, color = washington_born)) +
+  xlab("Seahawks Fan") +
+  ylab("Coffee Cups") +
+  ggtitle("Coffee Cups vs. Seahawks Fans vs. Born in Washington")
+  
+
+
+
